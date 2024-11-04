091d52055d29a9d289edca8bc48b76cf4406c4bb
suibian <3397709386@qq.com>
2024-11-05 01:12:23 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,101 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  <!-- <template> -->
+    <div class="pict">  <img src="https://s21.ax1x.com/2024/11/04/pAsF4de.jpg" alt="汉堡" class="pic">
+</div>
+<!-- </template> -->
+<div class="flower-container">
+  <div class="flower"></div>
+</div>
+  
+
+</header>
+  
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped>
+.pic{
+width: 200px;
+height: 200px;
+}
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+} 
+
+
+
+</style>
